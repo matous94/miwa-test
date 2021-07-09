157d6bcff72e2e8fff8f576affce42157832f990
Matouš Vencl <matousvencl@gmail.com>
2021-07-09 11:23:06 +0200

    change styles

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,10 +5,14 @@ const HomePage: React.FC = () => {
   return (
     <div className="center" style={{ display: 'flex' }}>
       <Link to="/characters" style={{ marginRight: '16px' }}>
-        <button type="button">Characters</button>
+        <button className="navigation-button" type="button">
+          Characters
+        </button>
       </Link>
       <Link to="/vehicles" style={{ marginLeft: '16px' }}>
-        <button type="button">Vehicles</button>
+        <button className="navigation-button" type="button">
+          Vehicles
+        </button>
       </Link>
     </div>
   );
