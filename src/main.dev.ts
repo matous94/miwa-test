7e5ba20b2d7e928edf2126ae484c7ad2055e3cc6
Matouš Vencl <matousvencl@gmail.com>
2021-07-09 11:48:48 +0200

    do not open dev tools on start

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -64,6 +64,7 @@ const createWindow = async () => {
     resizable: false,
     icon: getAssetPath('icon.png'),
     webPreferences: {
+      devTools: false,
       contextIsolation: false,
       nodeIntegration: true,
     },
